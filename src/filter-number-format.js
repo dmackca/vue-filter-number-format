99809c400048871ff157f15c52f65e0ea3b57234
DMack <dmack.ca@gmail.com>
2018-04-17 18:28:41 -0700

    fix spacing that vim broke 😳

--- src/filter-number-format.js
+++ src/filter-number-format.js
@@ -1,13 +1,13 @@
 /**
-* number formatting helper functions
-* can be used in vue filters
-*/
+ * number formatting helper functions
+ * can be used in vue filters
+ */
 import numeral from 'numeral';
 
 /**
-* apply a numeral.js formatting string to an input value
-* @param {Number} val input value to be changed
-* @param {String} [format='0,0'] numeral.js format string
-* @return {String} formatted output string
-*/
+ * apply a numeral.js formatting string to an input value
+ * @param {Number} val input value to be changed
+ * @param {String} [format='0,0'] numeral.js format string
+ * @return {String} formatted output string
+ */
 export default (val, format = '0,0') => numeral(val).format(format);
